4f792fb106144c418cbfdbf05622844ea5745ae7
Mildan Falah <mildanfalah@gmail.com>
2024-07-11 14:43:48 +0700

    first commit - latihan webpack

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import "./style/style.css";
+import moment from "moment";
+import $ from "jquery";
+
+const displayTime = () => {
+  moment.locale("id");
+  $(".time").text(moment().format("LTS"));
+  $(".date").text(moment().format("LL"));
+};
+
+const updateTime = () => {
+  displayTime();
+  setTimeout(updateTime, 1000);
+};
+
+updateTime();
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+
+      // Babel Loader
+      {
+        test: /\.js$/,
+        exclude: "/node_modules/",
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
